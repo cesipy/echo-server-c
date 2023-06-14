bdd22d2c2202d112c792d3f8d816e3b514b189bb
cedric <cedric.sillaber@gmail.com>
2023-06-14 08:26:15 +0200

    add signal handler to handle CTRL + C

--- server.c
+++ server.c
@@ -1,9 +1,3 @@
-/**
- * TODO:
- *  error handling
- *  add comments
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
@@ -11,11 +5,16 @@
 #include <netinet/ip.h>
 #include <string.h>
 #include <unistd.h>
+#include <signal.h>
 
 #define BUF_SIZE 1024
 
 void cleanup(int sockfd, int new_socket);
-int setup_socket(int port, struct sockaddr_in server, struct sockaddr_in client);
+int setup_socket(int port, struct sockaddr_in *server);
+void signal_handler(int signr);
+
+
+int sockfd, new_socket;
 
 int main(int argc, char* argv[])
 {
@@ -23,44 +22,40 @@ int main(int argc, char* argv[])
     { 
         fprintf(stderr ,"Incorrect usage! Usage: %s <port>\n", argv[0]);
         exit(EXIT_FAILURE);
-        }
-
+    }
 
     int port;
     char* endptr;
-    int sockfd;
     struct sockaddr_in server;
     struct sockaddr_in client;
     char buffer[BUF_SIZE];
-
 
     port = strtol(argv[1],&endptr, 10);
     if (*endptr != '\0')
     { 
         fprintf(stderr, "failed to convert number %s.\n", argv[1] );
         exit(EXIT_FAILURE);
-        }
+    }
 
-
-    sockfd = setup_socket(port, server, client);
+    sockfd = setup_socket(port, &server);
     printf("Server connected to port %d\n", port);
 
+    struct sigaction sa;
+    sa.sa_handler = signal_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+
+    if (sigaction(SIGINT, &sa, NULL) == -1) {
+        perror("sigaction");
+        return 1;
+    }
     
     int stop_flag = 0;
-    /**
-     * while loop waits for incoming connections
-     * as this is only single threaded, only one client at a time
-     * can connect to the server.
-     * 
-     * if stop_flag is set -> shutdown of the server
-     * stop_flag is set when a client types '/close'
-     */
     while(!stop_flag) 
     {
         socklen_t addrlen = sizeof(client);
 
-        // accept new incoming connection from sockfd
-        int new_socket = accept(sockfd, (struct sockaddr*)&client, &addrlen);
+        new_socket = accept(sockfd, (struct sockaddr*)&client, &addrlen);
         if (new_socket <0) 
         {
             perror("accept client");
@@ -68,10 +63,9 @@ int main(int argc, char* argv[])
         }
         printf("Connected!\n");
 
-        // loop for clonnection
         for (;;) 
         {
-            memset(buffer, 0, BUF_SIZE);    // reset bffer
+            memset(buffer, 0, BUF_SIZE);   
             int status_read = read(new_socket, buffer, BUF_SIZE - 1);
             if (status_read <0)
             {
@@ -88,8 +82,6 @@ int main(int argc, char* argv[])
                 stop_flag = 1;
                 break;
             }
-
-            // write echo message to client
             else 
             {
                 dprintf(new_socket, "echo: %s\n", buffer);
@@ -102,8 +94,7 @@ int main(int argc, char* argv[])
 }
 
 
-
-int setup_socket(int port, struct sockaddr_in server, struct sockaddr_in client) 
+int setup_socket(int port, struct sockaddr_in *server) 
 {
     int sockfd;
 
@@ -113,13 +104,13 @@ int setup_socket(int port, struct sockaddr_in server, struct sockaddr_in client)
         cleanup(sockfd, -1);
     }
 
-    memset(&server, 0, sizeof(struct sockaddr_in));
-    server.sin_family = AF_INET;
-    server.sin_port = htons(port);
-    server.sin_addr.s_addr = htonl(INADDR_ANY);
+    memset(server, 0, sizeof(struct sockaddr_in));
+    server->sin_family = AF_INET;
+    server->sin_port = htons(port);
+    server->sin_addr.s_addr = htonl(INADDR_ANY);
 
-    int bind_status = bind(sockfd, (struct sockaddr*)&server, 
-        sizeof(server));
+    int bind_status = bind(sockfd, (struct sockaddr*)server, 
+        sizeof(*server));
     if (bind_status == -1) 
     {
         perror("bind error");
@@ -142,4 +133,16 @@ void cleanup(int sockfd, int new_socket)
     if (sockfd != -1) 
         close(sockfd);
     exit(EXIT_FAILURE);
+}
+
+
+void signal_handler(int signr) 
+{
+    if (signr == SIGINT) 
+    {
+        char* message = "received CTRL + C. Shutting down the server!\n";
+        size_t message_len = strlen(message);
+        write(STDOUT_FILENO, message, message_len);
+        cleanup(sockfd, new_socket);
+    }
 }
